4f943bb205ac7ef769861a2b6e8785859d033eb3
kousha <kousha1367@gmail.com>
2016-11-26 18:20:57 -0800

    Adding i3 install

--- install.sh
+++ install.sh
@@ -19,10 +19,18 @@ cp zsh/zshrc ~/.zshrc
 cp zsh/dallas.zsh-theme ~/.oh-my-zsh/themes/dallas.zsh-theme
 
 
+# i3
+echo "Setting up i3"
+cd $current
+cp i3/config ~/.i3/config
+sudo cp i3/i3status.conf /etc/i3status.conf
+ 
+
 # xscreensaver
 echo "Setting up xscreensaver"
 sudo apt-get install --yes --force-yes xscreensaver xscreensaver-data-extra xscreensaver-gl-extra
 
 # Bashrc
 echo "Setting up bashrc"
+cd $current
 cp terminal/bashrc ~/.bashrc
